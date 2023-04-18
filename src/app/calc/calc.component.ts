91594805f7e6e67a1cd93ed6a3ba2cd9ab4ca21e
FelipeACamargo0907 <felipe.camargo0709@gmail.com>
2023-04-18 17:09:07 -0300

    Projeto calculadora finalizado

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./calc.component.css']
 })
 export class CalcComponent {
+  val1: number = 0;
+  val2: number = 0;
+  resultado: number = 0;
+
+
+  Calcular(op: string): number {
+    if (op == '+')
+      this.resultado = this.val1 + this.val2;
+    if (op == '-')
+      this.resultado = this.val1 - this.val2;
+    if (op == '*')
+      this.resultado = this.val1 * this.val2;
+    if (op == '/')
+      this.resultado = this.val1 / this.val2;
+
+    return this.resultado;
+  }
 
 }
